de3d246945199f49549b44e32bd1fcd549f25861
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-05 14:16:51 +0700

    first commit

---
+++ src/main/java/com/example/demopostman/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.demopostman.service.customer;
+
+import com.example.demopostman.model.Customer;
+import com.example.demopostman.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
